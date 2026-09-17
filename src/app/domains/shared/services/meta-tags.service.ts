08d35b0aa5c93105d8dc6c8c4ad46f689514ccc1
oscarbravo <oscarbravoarrieta@gmail.com>
2026-09-17 10:05:33 -0500

    30. Proceso de Hydration y Event Replay en Angular

--- src/app/domains/shared/services/meta-tags.service.ts
+++ src/app/domains/shared/services/meta-tags.service.ts
@@ -1,43 +1,42 @@
 import { inject, Service } from '@angular/core';
-import { Title, Meta, MetaDefinition } from '@angular/platform-browser'
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 import { environment } from '@environments/environment';
-
 export interface PageMetaData {
-    title: string
-    description: string
-    image: string
-    url: string
+    title: string;
+    description: string;
+    image: string;
+    url: string;
 }
 
 const defaultMetaData: PageMetaData = {
     title: 'ngStore',
     description: 'No Store is a store for Ng products',
-    image: 'http://picsum.photho/200/300',
-    url: environment.domain
+    image: 'http://picsum.photos/200/300',
+    url: environment.domain,
 }
-
 
 @Service()
 export class MetaTagsService {
-
-    titleService = inject(Title)
-    metaService = inject(Meta)
+    titleService = inject(Title);
+    metaService = inject(Meta);
 
     updtaMetaTags(metaData: Partial<PageMetaData>) {
         const metaDataToUpdate = {
             ...defaultMetaData,
-            ...metaData
-        }
+            ...metaData,
+        };
 
         const tags = this.generateMetaTagsDefinitions(metaDataToUpdate)
 
-        tags.forEach(tag => this.metaService.updateTag(tag))
+        tags.forEach((tag) => this.metaService.updateTag(tag));
 
         this.titleService.setTitle(metaData.title || defaultMetaData.title)
 
     }
 
-    private generateMetaTagsDefinitions(metaData: PageMetaData): MetaDefinition[] {
+    private generateMetaTagsDefinitions(
+        metaData: PageMetaData,
+    ): MetaDefinition[] {
         return [
             {
                 name: 'title',
@@ -45,25 +44,24 @@ export class MetaTagsService {
             },
             {
                 name: 'description',
-                content: metaData.description
+                content: metaData.description,
             },
             {
                 property: 'og:title',
-                content: metaData.title
+                content: metaData.title,
             },
             {
                 property: 'og:description',
-                content: metaData.description
-            },                                    
+                content: metaData.description,
+            },
             {
                 property: 'og:image',
-                content: metaData.image
+                content: metaData.image,
             },
             {
                 property: 'og:url',
-                content: metaData.url
+                content: metaData.url,
             },
-        ]
-
+        ];
     }
 }
